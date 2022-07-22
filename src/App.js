f76c10d7b38378ebfe646df28e99177adb5ba726
Mathieu Florentin <mathieu.florentin27@gmail.com>
2022-07-22 16:16:05 +0200

    Implement first routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./Dashboard/pages/Dashboard";
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </main>
+    </Router>
   );
+  // const [data, setData] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <p>{!data ? "Loading..." : data}</p>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
