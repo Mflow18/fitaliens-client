079b766c9e696569a84e5aeb03796baf529c9f58
Mathieu Florentin <mathieu.florentin27@gmail.com>
2023-04-14 18:41:41 +0200

    Reset package.json - configure Eslint - Switch to Npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
+// eslint-disable-next-line import/no-commonjs, unicorn/prefer-module, no-undef
 module.exports = {
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    tsconfigRootDir: __dirname,
-    sourceType: "module",
-  },
   ignorePatterns: [
     "node_modules",
     "dist",
@@ -12,22 +8,7 @@ module.exports = {
     "coverage",
     "jest.config.js",
   ],
-  plugins: [
-    "@typescript-eslint",
-    "github",
-    "jsx-a11y",
-    "prettier",
-    "sonarjs",
-    "functional",
-  ],
   extends: [
-    "plugin:react-hooks/recommended",
-    "hardcore",
-    "hardcore/react",
-    "hardcore/react-testing-library",
-    "hardcore/jest",
-    "hardcore/fp",
-    "hardcore/ts",
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
@@ -36,30 +17,15 @@ module.exports = {
     "plugin:sonarjs/recommended",
     "plugin:unicorn/recommended",
     "prettier",
-    "plugin:functional/lite",
-    "plugin:functional/stylistic",
   ],
-  env: {
-    browser: true,
-  },
-
+  plugins: ["@typescript-eslint", "github", "jsx-a11y", "prettier", "sonarjs"],
   overrides: [
     {
-      files: [
-        "server/**/*.ts",
-        "*.test.ts",
-        "*.test.tsx",
-        "*.spec.ts",
-        "*.spec.tsx",
-      ],
+      files: ["*.test.ts", "*.test.tsx", "*.spec.ts", "*.spec.tsx"],
       plugins: ["jest"],
-      extends: ["hardcore/node", "plugin:jest/recommended"],
+      extends: ["plugin:jest/recommended"],
       globals: {
         __dirname: true,
-        JSX: true,
-      },
-      env: {
-        browser: false,
       },
       rules: {
         "unicorn/prefer-module": "off", // To be able to use __dirname
@@ -68,25 +34,17 @@ module.exports = {
   ],
   rules: {
     // OFF
-    "@typescript-eslint/no-unused-vars": "off", //  handled by no-unused-vars
     "i18n-text/no-en": "off",
     "filenames/match-regex": "off", // handled by unicorn/filename-case
-    "import/named": "off", // doesnt work well
+    "import/named": "off", // doesn't work well
     "import/no-deprecated": "off", // takes too long
     "import/default": "off", // takes too long
+    "import/order": "off", // to avoid debate
     "import/no-unresolved": "off", // module bundle is vite
     "import/no-named-as-default-member": "off", // takes too long
     "import/no-namespace": "off", // enable namespaces
     "unicorn/no-null": "off",
-    "total-functions/require-strict-mode": "off", // too much of a mess with imports
-    "@typescript-eslint/naming-convention": "off", // annoying in interface declarations
-    "@typescript-eslint/prefer-readonly-parameter-types": "off", // overriden by functional/prefer-immutable-type
-    "react/forbid-component-props": "off", // too much of a mess
-    "import/no-unused-modules": "off", // takes too long
     "no-unused-vars": "off", // handled by @typescript-eslint/no-unused-vars
-    "functional/no-return-void": "off", // messes up with interfaces
-    "react/jsx-no-bind": "off", // outdated
-    "react-perf/jsx-no-new-function-as-prop": "off",
     // ERRORS
     "import/extensions": [
       "error",
